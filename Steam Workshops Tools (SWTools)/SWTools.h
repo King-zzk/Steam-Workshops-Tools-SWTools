b9937f77a4a0c5dfe760a2006c14f08c694e12a3
User <User@DESKTOP-MF6NDKC>
2025-01-21 16:40:29 +0800

    添加项目文件。

---
+++ Steam Workshops Tools (SWTools)/SWTools.h
@@ -0,0 +1,16 @@
+#pragma once 
+
+//必要的头文件
+#include <string>
+#include <iostream>
+#include <stdio.h>
+#include <stdlib.h>
+#include <windows.h>
+#include <conio.h>
+using namespace std;
+
+//函数声明
+void command(string arg);
+void help();
+void about();
+void SWTools();
