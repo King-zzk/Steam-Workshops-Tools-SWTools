2ab3d87005bc5aac9d275e731d0c5713bba03377
Commkom <zzk6396515@outlook.com>
2025-01-23 21:30:24 +0800

    修改一些bug

--- Steam Workshops Tools (SWTools)/Wallpaper.cpp
+++ Steam Workshops Tools (SWTools)/Wallpaper.cpp
@@ -17,29 +17,30 @@ void wallpaper() {
 	if (input.find_first_not_of("0123456789")!= string::npos) {
 		cout << "输入有误，请重新输入！" << endl;
 		wallpaper();
-	}
-	else {
-		//检查在steam文件夹是否有steamcmd.exe
-		int downloadResult = system(R"(cd .\Steam\ && curl -o "steamcmd.zip" "https://steamcdn-a.akamaihd.net/client/installer/steamcmd.zip" && tar -xzvf steamcmd.zip && del steamcmd.zip)");
-		if (downloadResult == 0) {
-		cout << "下载完成！" << endl;
-	}
-	else {
-		cout << "下载失败！(请检查网络连接)" << endl;
-		transmit();
+	} else {
+			//检查在steam文件夹是否有steamcmd.exe
+			if (_access(".\\Steam\\steamcmd.exe", 0) == -1) {
+			cout << "Steamcmd.exe未找到，正在下载..." << endl;
+			//下载steamcmd.exe
+			int downloadResult = system(R"(cd .\Steam\ && curl -o "steamcmd.zip" "https://steamcdn-a.akamaihd.net/client/installer/steamcmd.zip" && tar -xzvf steamcmd.zip && del steamcmd.zip)");
+			if (downloadResult == 0) {
+				cout << "下载完成！" << endl;
+			} else {
+				cout << "下载失败！(请检查网络连接)" << endl;
+				transmit();
+			}
+		}
 	}
 	//下载创意工坊文件
 	cout << "正在下载创意工坊文件中..." << endl;
-	string command = "start .\\Steam\\steamcmd.exe +login kzeon410 wnq69815I +workshop_download_item 431960 " + input + "exit";
+	string command = ".\\Steam\\steamcmd.exe +login kzeon410 wnq69815I +workshop_download_item 431960 " + input + " +quit";
 	system(command.c_str()); // 这行代码会等待steamcmd.exe执行完毕
-	cout << "如果steamcmd.exe关闭窗口，请在这里按任意键继续..." << endl;
-	cin.get();
 	//打开创意工坊文件夹
 	string path = ".\\Steam\\steamapps\\workshop\\content\\431960\\" + input + "\\";
 	// 使用正确的命令格式来打开文件夹
 	string explorerCommand = "explorer.exe " + path;
 	system(explorerCommand.c_str()); // 这行代码会等待explorer.exe执行完毕
-	cout << "下载完成！(下载可能会失败，请重试几遍！)" << endl;
+	cout << "下载完成！(如果出现timeout，请重试！)" << endl;
 	transmit();
-	}
 }
+
