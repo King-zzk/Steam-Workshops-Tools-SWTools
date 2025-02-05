739b0ca684e696ca07e451704f0bb375fb5b077f
Commkom <zzk6396515@outlook.com>
2025-02-05 23:00:42 +0800

    以后在制作钢铁雄心4模组下载吧...

--- Steam Workshops Tools (SWTools)/help.cpp
+++ Steam Workshops Tools (SWTools)/help.cpp
@@ -4,7 +4,6 @@ void help()
 {
 	cout << endl;
 	cout << "wallpaper            下载Wallpaper Engine的创意工坊文件" << endl;
-	cout << "Hoi4                 下载钢铁雄心4的创意工坊文件" << endl;
 	cout << "clear                                       清屏" << endl;
 	cout << "about                                       关于" << endl;
 	cout << "update                                      检查更新" << endl;
