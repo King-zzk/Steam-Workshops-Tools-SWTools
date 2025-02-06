020d4ac3984b1f258348fe05ac14b4eecb59b96a
Commkom <zzk6396515@outlook.com>
2025-02-06 12:54:57 +0800

    改bug！

---
+++ Steam Workshops Tools (SWTools)/main.cpp
@@ -0,0 +1,14 @@
+#include "SWTools.h"
+
+int main() {
+    cout << "Commkom(king-zzk) Steam Workshops Tools (SWTools)[版本 0.1.2(2025.02)]" << endl;
+    cout << "(c) Commkom(king-zzk) 2025 All rights reserved。" << endl;
+    cout << "请注意：本软件完全免费使用，请勿用于倒卖或盈利目的，否则后果自负！" << endl;
+    cout << endl;
+    transmit();
+}
+
+void goto_transmit()
+{
+    transmit();
+}
