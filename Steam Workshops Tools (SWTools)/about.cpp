b647b682d8a87543092e4a0f433e7fd7cff28b2e
Zhu <318406737@qq.com>
2025-02-06 12:02:01 +0800

    Update about.cpp

--- Steam Workshops Tools (SWTools)/about.cpp
+++ Steam Workshops Tools (SWTools)/about.cpp
@@ -4,7 +4,7 @@ void about() {
     cout << endl;
     cout << "Steam Workshops Tools SWTools （version 2025.2）" << endl;
     cout << "Copyright © 2024-2025 SWTools. All rights reserved." << endl;
-    cout << " \033[31m这是一个完全免费的Steam Workshops 下载工具，请勿倒卖！\033[0m" << endl;
+    cout << " 这是一个完全免费的Steam Workshops 下载工具，请勿倒卖！" << endl;
     cout << "作者：king-Zzk（Commkom）" << endl;
     cout << "github：https://github.com/King-zzk/Steam-Workshops-Tools-SWTools" << endl;
     cout << endl;
