d19733d40a1015faa24df1494168b1296e5e402e
Commkom <zzk6396515@outlook.com>
2025-02-06 12:57:04 +0800

    help!

--- Steam Workshops Tools (SWTools)/help.cpp
+++ Steam Workshops Tools (SWTools)/help.cpp
@@ -4,11 +4,12 @@ void help()
 {
 	cout << endl;
 	cout << "hoi4										  下载钢铁雄心4的创意工坊文件" << endl;
-	cout << "wallpaper									  下载Wallpaper Engine的创意工坊文件" << endl;
+	cout << "wallpaper									  下载壁纸引擎的创意工坊文件" << endl;
 	cout << "clear										  清屏" << endl;
 	cout << "about										  关于" << endl;
 	cout << "exit										  退出" << endl;
 	cout << "help										  帮助" << endl;
+	cout << endl;
 	cout << "有关更多信息，请访问github主页 https://github.com/King-zzk/Steam-Workshops-Tools-SWTools" << endl;
 	cout << endl;
 	transmit();
