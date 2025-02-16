c31b843b9c08a26b4315b78e6d1ab45b06d4fd26
Commkom <zzk6396515@outlook.com>
2025-02-16 11:32:08 +0800

    dll

--- Steam Workshops Tools (SWTools)/about.cpp
+++ Steam Workshops Tools (SWTools)/about.cpp
@@ -2,10 +2,10 @@
 
 void about() {
     cout << endl;
-    cout << "Steam Workshops Tools SWTools （version 2025.2.01）" << endl;
+    cout << "Steam Workshops Tools SWTools version 2025.2.02" << endl;
     cout << "Copyright © 2024-2025 SWTools. All rights reserved." << endl;
     cout << " 这是一个完全免费的Steam Workshops 下载工具，请勿倒卖！" << endl;
-    cout << "作者：king-Zzk（Commkom）" << endl;
+    cout << "作者：king-Zzk Commkom" << endl;
     cout << "github：https://github.com/King-zzk/Steam-Workshops-Tools-SWTools" << endl;
     cout << endl;
     transmit();
