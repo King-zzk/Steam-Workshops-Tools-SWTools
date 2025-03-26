32d3fd1343370a9519456ec2fa678a5b5fa0c513
Kingofzzk <zzk6396515@outlook.com>
2025-03-26 17:49:23 +0800

    更改help

--- Steam Workshops Tools (SWTools)/help.cpp
+++ Steam Workshops Tools (SWTools)/help.cpp
@@ -2,15 +2,14 @@
 
 void help()
 {
-	cout << endl;
-	cout << "hoi4										  下载钢铁雄心4的创意工坊文件" << endl;
-	cout << "gmod										  下载Garry's Mod的创意工坊文件" << endl;
-	cout << "wallpaper									  下载壁纸引擎的创意工坊文件" << endl;
-	cout << "clear										  清屏" << endl;
-	cout << "about										  关于" << endl;
-	cout << "exit										  退出" << endl;
-	cout << "help										  帮助" << endl;
-	cout << endl;
+	cout << "欢迎使用Steam Workshops Tools！" << endl;
+	cout << "hoi4      下载钢铁雄心4的创意工坊文件" << endl;
+	cout << "gmod      下载Garry's Mod的创意工坊文件" << endl;
+	cout << "wallpaper 下载壁纸引擎的创意工坊文件" << endl;
+	cout << "clear	   清屏" << endl;
+	cout << "about     关于" << endl;
+	cout << "exit	   退出" << endl;
+	cout << "help	   帮助" << endl;
 	cout << "有关更多信息，请访问github主页 https://github.com/King-zzk/Steam-Workshops-Tools-SWTools" << endl;
 	cout << endl;
 	transmit();
