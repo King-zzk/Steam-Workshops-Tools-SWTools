b9b863b6c2b1e0ff504e2dd042504568a4fd5fa7
Kingofzzk <zzk6396515@outlook.com>
2025-03-26 17:50:48 +0800

    字体

--- Steam Workshops Tools (SWTools)/about.cpp
+++ Steam Workshops Tools (SWTools)/about.cpp
@@ -3,7 +3,7 @@
 void about() {
     cout << endl;
     cout << "Steam Workshops Tools SWTools version 2025.3.26" << endl;
-    cout << "Copyright © 2024-2025 SWTools. All rights reserved." << endl;
+    cout << "Copyright @ 2024-2025 SWTools. All rights reserved." << endl;
     cout << " 这是一个完全免费的Steam Workshops 下载工具，请勿倒卖！" << endl;
     cout << "作者：king-Zzk Commkom" << endl;
     cout << "github：https://github.com/King-zzk/Steam-Workshops-Tools-SWTools" << endl;
