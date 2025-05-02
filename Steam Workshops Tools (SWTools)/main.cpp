f1c76ffb27b308c8fc602c935df871d95d67b535
朱梓康 <zzk6396515@outlook.com>
2025-05-02 10:16:55 +0800

    添加更新功能以检查版本信息
    在 `SWTools.h` 中引入 `urlmon.h` 并声明 `update()` 函数。
    更新项目文件以包含 `update.cpp`。
    在 `main.cpp` 中替换 `useragreement()` 调用为 `update()`。
    实现 `update()` 函数，从指定 URL 下载版本信息并进行比较，输出相应提示。

--- Steam Workshops Tools (SWTools)/main.cpp
+++ Steam Workshops Tools (SWTools)/main.cpp
@@ -5,7 +5,7 @@ int main() {
     cout << "(c) Commkom(king-zzk) 2025 All rights reserved。" << endl;
     cout << "请注意：本软件完全免费使用，请勿用于倒卖或盈利目的，否则后果自负！" << endl;
     cout << endl;
-    useragreement();
+    update();
 }
 
 void goto_transmit()
---
+++ Steam Workshops Tools (SWTools)/update.cpp
@@ -0,0 +1,31 @@
+#include "SWTools.h"
+
+void update() {
+	cout << "检查更新中..." << endl;
+	string line;
+	const wchar_t* url = L"https://king-zzk.github.io/version.txt";
+	const wchar_t* path = L"C:\\Windows\\Temp\\version.txt";
+	// 下载文件
+	HRESULT hr = URLDownloadToFileW(NULL, url, path, 0, NULL);
+	if (SUCCEEDED(hr)) {
+		ifstream input("C:\\Windows\\Temp\\version.txt");
+		if (input.is_open()) {
+			while (getline(input, line)) {
+				if (line == "0.1.8") {
+					cout << "当前版本为最新版本" << endl;
+					useragreement();
+				}
+				else {
+					cout << "当不是最新版本，最新版本为：" << line << endl;
+					cout << "请下载最新版本" << endl;
+					useragreement();
+				}
+			}
+		}
+	}
+	else {
+		cout << "检查失败(检查是否打开加速器，没有请打开！)" << endl;
+		cout << "错误码：0x" << hex << endl;
+		useragreement();
+	}
+}
