4f403bcbfecff6b5febd726dd54a4fa66ef93b0c
king-zzk <zzk6396515@outlook.com>
2025-05-02 15:29:29 +0800

    改进更新

--- Steam Workshops Tools (SWTools)/update.cpp
+++ Steam Workshops Tools (SWTools)/update.cpp
@@ -2,22 +2,29 @@
 
 void update() {
 	cout << "检查更新中..." << endl;
+	if (_access  (".\\version.txt" ,0) == 0) {
+		remove ("version.txt");
+	}
 	string line;
 	const wchar_t* url = L"https://king-zzk.github.io/version.txt";
-	const wchar_t* path = L"C:\\Windows\\Temp\\version.txt";
+	const wchar_t* path = L".\\version.txt";
 	// 下载文件
 	HRESULT hr = URLDownloadToFileW(NULL, url, path, 0, NULL);
 	if (SUCCEEDED(hr)) {
-		ifstream input("C:\\Windows\\Temp\\version.txt");
+		ifstream input(".\\version.txt");
 		if (input.is_open()) {
 			while (getline(input, line)) {
 				if (line == "0.1.9") {
 					cout << "当前版本为最新版本" << endl;
+					input.close();
+					remove ("version.txt");
 					useragreement();
 				}
 				else {
-					cout << "当不是最新版本，最新版本为：" << line << endl;
+					cout << "当前不是最新版本，最新版本为：" << line << endl;
 					cout << "请下载最新版本" << endl;
+					input.close();
+					remove ("version.txt");
 					useragreement();
 				}
 			}
