0a1ed9e208391361310854fb77d14e87caacdd52
king-zzk <zzk6396515@outlook.com>
2025-05-17 15:56:42 +0800

    彻底删除颜色

--- Steam Workshops Tools (SWTools)/main.cpp
+++ Steam Workshops Tools (SWTools)/main.cpp
@@ -29,8 +29,8 @@ int main() {
 	try {
 		check_update();
 	} catch (exception e) {
-		cout << "\033[31m错误: " << e.what() << "\033[0m" << endl;
-		cout << "\033[33m警告: 正在跳过更新检查\033[0m" << endl;
+		cout << "错误: " << e.what() << endl;
+		cout << "警告: 正在跳过更新检查" << endl;
 	}
 	cout << endl;
 
@@ -41,7 +41,7 @@ int main() {
 	}
 
 	// 命令循环
-	cout << "\033[36m" << text::usage << "\033[0m" << endl;
+	cout << text::usage  << endl;
 	while (command());
 	return 0;
 }
