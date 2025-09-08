56dbefe03d33ed6075f23ef8b9f1e4a7ecd91a36
king-zzk <commkom@qq.com>
2025-09-08 19:49:29 +0800

    修复界面问题，支持自定义背景（“有一些Bug”）

--- SWTools.WPF/AddTaskWindow.xaml.cs
+++ SWTools.WPF/AddTaskWindow.xaml.cs
@@ -24,5 +24,15 @@ namespace SWTools.WPF {
         private void BtnOk_Click(object sender, RoutedEventArgs e) {
             Close();
         }
+
+        private void BtnHelpItem_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("物品id就是你要下载的MOD，你要下载的物品ID在MOD的网址上/?id=XXXXXXXX，XXXXXXXX就是id编号","什么是物品ID？",MessageBoxButton.OK);
+        }
+
+        private void BtnHelpApp_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("APPID就是你要下载的MOD所属游戏或工具，你要下载的物品的所属游戏或工具Steam购买网址,比如：https://store.steampowered.com/app/XXXXXX/GAME/,这个XXXXXX就是APP的ID！", "什么是APPID？", MessageBoxButton.OK);
+        }
     }
 }
--- SWTools.WPF/MoreWindow.xaml.cs
+++ SWTools.WPF/MoreWindow.xaml.cs
@@ -1,7 +1,8 @@
 ﻿using System;
+using System.IO;
+using System.Text;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
@@ -12,23 +13,74 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
 
+
 namespace SWTools.WPF {
     /// <summary>
     /// MoreWindow.xaml 的交互逻辑
     /// </summary>
-    public partial class MoreWindow : Window {
-        public MoreWindow() {
+    public partial class MoreWindow : Window
+    {
+        public MoreWindow()
+        {
             InitializeComponent();
             VersionText.Text = $"这是 Steam Workshop Tools v{SWTools.Core.Helper.GetVersionStr()}。";
+
+            if (File.Exists("./background.dat"))
+            {
+                // 读取 background.dat 文件内容作为背景图片路径
+                string backgroundfile;
+                using (FileStream F = new FileStream("./background.dat", FileMode.Open, FileAccess.Read, FileShare.Read))
+                {
+                    byte[] buffer = new byte[F.Length];
+                    F.ReadExactly(buffer);
+                    backgroundfile = Encoding.UTF8.GetString(buffer);
+                    F.Close();
+                }
+                this.Background = new ImageBrush(new BitmapImage(new Uri(backgroundfile, UriKind.Absolute)));
+            }
         }
 
-        private void BtnOk_Click(object sender, RoutedEventArgs e) {
+        private void BtnOk_Click(object sender, RoutedEventArgs e)
+        {
             Close();
         }
 
-        private void BtnGithub_Click(object sender, RoutedEventArgs e) {
+        private void BtnGithub_Click(object sender, RoutedEventArgs e)
+        {
             System.Diagnostics.Process.Start("explorer.exe",
                 "https://github.com/King-zzk/Steam-Workshops-Tools-SWTools");
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+            // 获取文本框内容
+            string? backgroundfile = backgroundtextbox?.Text;
+
+            if (string.IsNullOrWhiteSpace(backgroundfile))
+            {
+                MessageBox.Show("请输入文件地址！");
+                return;
+            }
+
+            if (!File.Exists(backgroundfile))
+            {
+                MessageBox.Show("文件不存在，请检查路径是否正确！");
+                return;
+            }
+
+            try
+            {
+                // 存储文件地址 
+                FileStream F = new FileStream("background.dat",
+                    FileMode.OpenOrCreate, FileAccess.ReadWrite);
+                F.Write(Encoding.UTF8.GetBytes(backgroundfile));
+                this.Background = new ImageBrush(new BitmapImage(new Uri(backgroundfile, UriKind.Absolute)));
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"设置背景失败: {ex.Message}");
+            }
         }
     }
 }
