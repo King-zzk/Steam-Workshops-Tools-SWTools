1ad241ed139a4eca108326471f2042f433d5624d
king-zzk <commkom@qq.com>
2025-09-08 19:49:29 +0800

    修复界面问题，支持自定义背景（“有一些Bug”）

--- SWTools.WPF/MainWindow.xaml.cs
+++ SWTools.WPF/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System.Text;
+﻿using System.IO;
+using System.Text;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -13,9 +14,23 @@ namespace SWTools.WPF {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
+    
     public partial class MainWindow : Window {
         public MainWindow() {
             InitializeComponent();
+            if (File.Exists("./background.dat"))
+            {
+                // 读取 background.dat 文件内容作为背景图片路径
+                string backgroundfile;
+                using (FileStream F = new FileStream("./background.dat", FileMode.Open, FileAccess.Read, FileShare.Read))
+                {
+                    byte[] buffer = new byte[F.Length];
+                    F.ReadExactly(buffer);
+                    backgroundfile = Encoding.UTF8.GetString(buffer);
+                    F.Close();
+                }
+                this.Background = new ImageBrush(new BitmapImage(new Uri(backgroundfile, UriKind.Absolute)));
+            }
         }
 
         private void BtnAddTask_Click(object sender, RoutedEventArgs e) {
@@ -26,6 +41,11 @@ namespace SWTools.WPF {
         private void BtnMore_Click(object sender, RoutedEventArgs e) {
             MoreWindow moreWindow = new MoreWindow();
             moreWindow.ShowDialog();
+        }
+
+        private void ListView_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
         }
     }
 }
